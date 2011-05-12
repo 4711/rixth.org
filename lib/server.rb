7632814aae54cd03f2e34bc78d5ed1ad9049f466
Tom Rix <tom@rixth.org>
2011-05-12 00:11:54 -0700

    new site "done"

---
+++ lib/server.rb
@@ -0,0 +1,40 @@
+require 'sinatra/base'
+require 'json'
+require 'net/http'
+
+module RixthOrg
+  class Server < Sinatra::Base
+    set :cache, Dalli::Client.new('localhost:11211', {
+      :expires_in => 5 * 60
+    })
+    
+    set :public, File.dirname(__FILE__) + '/public'
+  
+  
+    get '/' do
+      @git = my_git_repos
+      haml :index
+    end
+  
+    helpers do
+      def style_github_repo(repo)
+        "<a href=\"#{repo['url']}\">#{repo['name']}</a>"
+      end
+    end
+  
+    def my_git_repos
+      from_cache = settings.cache.get('gitrepos')
+      return JSON.parse(from_cache) if from_cache
+      
+      begin
+        repositories = JSON.parse(Net::HTTP.get_response(URI.parse('http://github.com/api/v2/json/repos/show/rixth')).body)["repositories"]
+      rescue
+        return []
+      end
+    
+      repositories.reject! { |repo| repo['watchers'] < 3 }
+
+      settings.cache.set('gitrepos', JSON.generate(repositories))
+    end
+  end
+end
