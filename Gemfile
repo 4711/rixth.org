07cca2f1024b49ea24a72c6cbfc8917279087c32
Thomas Rix <tom@rixth.org>
2012-06-03 17:35:42 -0700

    new look

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
 source "http://rubygems.org"
 
+gem 'rdiscount'
 gem 'sinatra'
 gem 'haml'
 gem 'dalli'
 gem 'json'
+
+group :development do
+  gem 'heroku'
+end
--- lib/server.rb
+++ lib/server.rb
@@ -6,29 +6,29 @@ module RixthOrg
     set :cache, Dalli::Client.new(nil, {
       :expires_in => 3600 # cache for an hour
     })
-    
-    set :public, File.dirname(__FILE__) + '/../public'
+
+    set :public_folder, File.dirname(__FILE__) + '/../public'
     set :haml, :format => :html5
-    
+
     before do
       redirect request.url.sub('//www.', '//') if request.host[0..2] == 'www'
     end
-  
+
     get '/' do
       @git = my_git_repos
       haml :index
     end
-  
+
     def my_git_repos
       from_cache = settings.cache.get('gitrepos')
       return JSON.parse(from_cache) if from_cache
-      
+
       begin
         repositories = JSON.parse(Net::HTTP.get_response(URI.parse('http://github.com/api/v2/json/repos/show/rixth')).body)["repositories"]
       rescue
         return []
       end
-    
+
       repositories.reject! { |repo| repo['watchers'] < 3 }
       repositories.sort! { |a, b| b['watchers'] <=> a['watchers'] }
 
